d8c89b81ef91ca586048be8ec758ba3efd5f401a
KunalSingh19 <73868343+KunalSingh19@users.noreply.github.com>
2025-10-02 15:18:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -58,7 +58,6 @@ const SCRAPE_SOURCES = [
   'https://raw.githubusercontent.com/clarketm/proxy-list/master/proxy-list-raw.txt', // Mixed ip:port and protocol
   'https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/http.txt', // Plain ip:port
   'https://raw.githubusercontent.com/hookzof/socks5_list/master/proxy.txt', // Plain ip:port (HTTP subset)
-  'https://www.proxy-list.download/api/v1/get?type=http', // Plain ip:port
   'https://cdn.jsdelivr.net/gh/proxifly/free-proxy-list@main/proxies/protocols/http/data.txt',
   'https://api.proxyscrape.com/v4/free-proxy-list/get?request=display_proxies&protocol=http&proxy_format=protocolipport&format=text&timeout=20000',
   'https://cdn.jsdelivr.net/gh/mzyui/proxy-list@main/all.txt',
